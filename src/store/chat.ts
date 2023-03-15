dc93c8b9fcf49202496077a8363984017f072b16
WesleyMaik <wesleynamikaze404@gmail.com>
2023-03-15 16:37:01 -0400

    :sparkles: feat add addChat and editChat functions

--- src/store/chat.ts
+++ src/store/chat.ts
@@ -1,9 +1,12 @@
 import { create } from "zustand";
+import { v4 } from 'uuid';
 
 export interface UseChatProps {
     chat: Chat[],
     selectedChat: Chat | undefined,
     setChat: (payload: Chat) => void,
+    addChat: () => void,
+    editChat: (id: string, payload: Partial<Chat>) => void,
     setSelectedChat: (payload: { id: string }) => void,
     removeChat: (pyload: { id: string }) => void,
     clearAll: () => void,
@@ -53,10 +56,33 @@ const initialChatState: Chat[] = [
     }
 ];
 
-export const useChat = create<UseChatProps>((set) => ({
+export const useChat = create<UseChatProps>((set, get) => ({
     chat: initialChatState,
     selectedChat: initialChatState[0],
     setChat: async (payload) => set(({ chat }) => ({ chat: [...chat, payload] })),
+    addChat: async () => {
+        const hasNewChat = get().chat.find(({ content }) => (content.length === 0));
+
+        if (!hasNewChat) {
+            const id = v4()
+            get().setChat({
+                role: "New chat",
+                id: id,
+                content: []
+            });
+            get().setSelectedChat({ id });
+        } else {
+            const { id } = hasNewChat;
+            get().setSelectedChat({ id });
+        };
+    },
+    editChat: async (id, payload) => set(({ chat }) => {
+        const newChatIndex = chat.findIndex((query) => (query.id === id));
+        if (newChatIndex > -1) {
+            chat[newChatIndex] = { ...chat[newChatIndex], ...payload };
+        };
+        return ({ chat, selectedChat: chat[newChatIndex] })
+    }),
     setSelectedChat: async (payload) => set(({ chat }) => {
         const selectedChat = chat.find(({ id }) => id === payload.id);
         return ({ selectedChat: selectedChat })
