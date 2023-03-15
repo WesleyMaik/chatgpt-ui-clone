b73a7f0d41d77f382f0f580141df76afea292ea0
WesleyMaik <wesleynamikaze404@gmail.com>
2023-03-15 14:41:58 -0400

    :sparkles: feat add Sidebar and Chat components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,22 @@
+//Components
+import { Stack, useMediaQuery } from "@chakra-ui/react";
+import { Sidebar } from "@/components/Sidebar";
+import { Chat } from "@/components/Chat";
+
 export default function Home() {
+    const [isResponsive] = useMediaQuery('(max-width: 800px)')
+
     return (
-        <></>
+        <Stack
+            direction={!isResponsive ? "row" : "column"}
+            width="full"
+            height="full"
+            spacing={0}
+        >
+            <Sidebar
+                isResponsive={isResponsive}
+            />
+            <Chat />
+        </Stack>
     );
 };
