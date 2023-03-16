421c2c8c52d744f48e7bdd06594c63d50bc520db
WesleyMaik <wesleynamikaze404@gmail.com>
2023-03-16 15:22:11 -0400

    :sparkles: feat add React-Query provider

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -4,13 +4,17 @@ import { PropsWithChildren } from "react";
 //Components
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@/styles/theme";
+import { QueryClientProvider } from "react-query";
+import { query } from "@/services/query";
 
 export interface IProvidersProps extends PropsWithChildren { };
 
 export const Providers = ({ children, ...props }: IProvidersProps) => {
     return (
-        <ChakraProvider theme={theme}>
-            {children}
-        </ChakraProvider>
+        <QueryClientProvider client={query}>
+            <ChakraProvider theme={theme}>
+                {children}
+            </ChakraProvider>
+        </QueryClientProvider>
     )
 };
