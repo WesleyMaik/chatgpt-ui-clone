f806d5b0ed20fbdad6714e2398ad2f5243358714
WesleyMaik <wesleynamikaze404@gmail.com>
2023-03-16 15:24:30 -0400

    :sparkles: feat add ChatGPT api and fix some styles

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,23 +1,27 @@
 //Modules
-import gptAvatar from "@/assets/gpt-avatar.svg"
+import gptAvatar from "@/assets/gpt-avatar.svg";
+import warning from "@/assets/warning.svg";
+import user from "@/assets/user.png";
+import { useRef } from "react";
 import { useChat } from "@/store/chat";
 import { useForm } from "react-hook-form";
 import { useAutoAnimate } from "@formkit/auto-animate/react"
+import { OpenAIApi, Configuration } from "openai";
+import { useMutation } from "react-query";
 
 //Components
 import { Input } from "@/components/Input";
-import { IconType } from "react-icons";
-import { FiAlertTriangle, FiSend, FiSun, FiZap } from "react-icons/fi";
+import { FiSend } from "react-icons/fi";
 import {
     Avatar,
-    Button,
-    Heading,
-    Icon,
     IconButton,
-    Spacer,
+    Spinner,
     Stack,
     Text
 } from "@chakra-ui/react";
+import ReactMarkdown from 'react-markdown'
+import { Instructions } from "../Layout/Instructions";
+import { useAPI } from "@/store/api";
 
 export interface ChatProps { };
 
@@ -25,47 +29,103 @@ interface ChatSchema {
     input: string
 };
 
-type Introdution = {
-    icon: IconType,
-    name: "Examples" | "Capabilities" | "Limitations",
-    list: string[]
-};
-
 export const Chat = ({ ...props }: ChatProps) => {
-    const { selectedChat, setSelectedChat } = useChat();
-    const { register, setValue } = useForm<ChatSchema>();
+    const { api } = useAPI();
+    const {
+        selectedChat,
+        addMessage,
+        addChat,
+        editChat
+    } = useChat();
+    const selectedId = selectedChat?.id,
+        selectedRole = selectedChat?.role;
+
+    const hasSelectedChat = selectedChat && selectedChat?.content.length > 0;
+
+    const {
+        register,
+        setValue,
+        handleSubmit
+    } = useForm<ChatSchema>();
+
+    const overflowRef = useRef<HTMLDivElement>(null);
+    const updateScroll = () => {
+        overflowRef.current?.scrollTo(0, overflowRef.current.scrollHeight);
+    };
 
     const [parentRef] = useAutoAnimate();
 
-    const introdution: Introdution[] = [
-        {
-            icon: FiSun,
-            name: "Examples",
-            list: [
-                "Explain quantum computing in simple terms",
-                "Got any creative ideas for a 10 year old's birthday?",
-                "How do i make an HTTP request in JavaScript?"
-            ]
-        },
-        {
-            icon: FiZap,
-            name: "Capabilities",
-            list: [
-                "Remembers what user said earlier in the conversation",
-                "Allows user to provide follow-up corrections",
-                "Trained to decline inappropriate requests"
-            ]
-        },
-        {
-            icon: FiAlertTriangle,
-            name: "Limitations",
-            list: [
-                "May occasionally generate incorrect information",
-                "May occasionally produce harmful instructions or biased content",
-                "Limited knowledge of world and events after 2021"
-            ]
-        }
-    ];
+    const configuration = new Configuration({
+        apiKey: api
+    });
+
+    const openAi = new OpenAIApi(configuration);
+
+    const { mutate, isLoading } = useMutation({
+        mutationKey: 'prompt',
+        mutationFn: async (prompt: string) => await openAi.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            max_tokens: 256,
+            messages: [{ role: 'user', content: prompt }]
+        })
+    });
+
+    const handleAsk = async ({ input: prompt }: ChatSchema) => {
+        updateScroll();
+        const sendRequest = (selectedId: string) => {
+            setValue("input", "");
+
+            addMessage(selectedId, {
+                emitter: "user",
+                message: prompt
+            });
+
+            mutate(prompt, {
+                onSuccess({ status, data }, variable) {
+                    if (status === 200) {
+                        const message = String(data.choices[0].message?.content);
+                        addMessage(selectedId, {
+                            emitter: "gpt",
+                            message
+                        });
+
+                        if (selectedRole == "New chat" || selectedRole == undefined) {
+                            editChat(selectedId, { role: variable });
+                        };
+                    }
+                    updateScroll();
+                },
+                onError(error) {
+                    type Error = {
+                        response: {
+                            data: {
+                                error: {
+                                    code: "invalid_api_key" | string,
+                                    message: string
+                                };
+                            },
+                        },
+                    };
+
+                    const { response } = error as Error,
+                        message = response.data.error.message;
+                    addMessage(selectedId, {
+                        emitter: "error",
+                        message
+                    });
+                    updateScroll();
+                }
+            });
+        };
+
+        if (selectedId) {
+            if (prompt && !isLoading) {
+                sendRequest(selectedId);
+            };
+        } else {
+            addChat(sendRequest);
+        };
+    };
 
     return (
         <Stack
@@ -75,112 +135,108 @@ export const Chat = ({ ...props }: ChatProps) => {
             <Stack
                 maxWidth="768px"
                 width="full"
-                height="full"
                 marginX="auto"
+                height="85%"
+                overflow="auto"
+                ref={overflowRef}
             >
                 <Stack
                     spacing={2}
-                    height="full"
                     padding={2}
                     ref={parentRef}
+                    height="full"
                 >
-                    {(selectedChat && selectedChat?.content.length > 0) ? (
+                    {(hasSelectedChat) ? (
                         selectedChat.content.map(({ emitter, message }, key) => {
+                            const getAvatar = () => {
+                                switch (emitter) {
+                                    case "gpt":
+                                        return gptAvatar;
+                                    case "error":
+                                        return warning;
+                                    default:
+                                        return user;
+                                }
+                            };
+
+                            const getMessage = () => {
+                                if (message.slice(0, 2) == "\n\n") {
+                                    return message.slice(2, Infinity);
+                                };
+
+                                return message;
+                            };
+
                             return (
                                 <Stack
                                     key={key}
                                     direction="row"
-                                    alignItems="center"
                                     padding={4}
                                     rounded={8}
                                     backgroundColor={
                                         (emitter == 'gpt') ? ("blackAlpha.200") : ("transparent")
                                     }
+                                    spacing={4}
                                 >
                                     <Avatar
                                         name={emitter}
-                                        src={
-                                            (emitter == 'gpt') ? (gptAvatar) : (undefined)
-                                        }
+                                        src={getAvatar()}
                                     />
-                                    <Text>{message}</Text>
+                                    <Text
+                                        whiteSpace="pre-wrap"
+                                        marginTop=".75em !important"
+                                        overflow="hidden"
+                                    >
+                                        <ReactMarkdown >
+                                            {getMessage()}
+                                        </ReactMarkdown>
+                                    </Text>
                                 </Stack>
                             )
                         })
                     ) : (
-                        <Stack
-                            justifyContent="center"
-                            alignItems="center"
-                            height="full"
-                        >
-                            <Heading
-                                size="lg"
-                                marginY={8}
-                            >ChatGPT</Heading>
-                            <Stack
-                                direction="row"
-                            >
-                                {introdution.map(({ icon, list, name }, key) => {
-                                    const handleClick = (text: string) => {
-                                        if (name == 'Examples') {
-                                            return () => setValue('input', text);
-                                        };
-                                        return undefined;
-                                    };
-
-                                    return (
-                                        <Stack
-                                            key={key}
-                                            alignItems="center"
-                                        >
-                                            <Icon
-                                                as={icon}
-                                            />
-                                            <Heading size="sm">{name}</Heading>
-                                            {list.map((text, key) => (
-                                                <Button
-                                                    key={key}
-                                                    maxWidth={64}
-                                                    height="fit-content"
-                                                    padding={4}
-                                                    onClick={handleClick(text)}
-                                                >
-                                                    <Text
-                                                        overflow="hidden"
-                                                        whiteSpace="normal"
-                                                    >{text}</Text>
-                                                </Button>
-                                            ))}
-                                        </Stack>
-                                    )
-                                })}
-                            </Stack>
-                        </Stack>
+                        <Instructions
+                            onClick={(text) => setValue('input', text)}
+                        />
                     )}
                 </Stack>
             </Stack>
-            <Spacer />
             <Stack
-                backgroundColor="blackAlpha.400"
+                height="20%"
                 padding={4}
+                backgroundColor="blackAlpha.400"
+                justifyContent="center"
+                alignItems="center"
+                overflow="hidden"
             >
-                <Input
-                    autoFocus={true}
-                    variant="filled"
-                    inputRightAddon={(
-                        <IconButton
-                            aria-label="send_button"
-                            icon={<FiSend />}
-                            backgroundColor="transparent"
-                        />
-                    )}
-                    {...register('input')}
-                />
-                <Text
-                    textAlign="center"
-                    fontSize="sm"
-                    opacity={.5}
-                >Free Research Preview. Our goal is to make AI systems more natural and safe to interact with. Your feedback will help us improve.</Text>
+                <Stack
+                    maxWidth="768px"
+                >
+                    <Input
+                        autoFocus={true}
+                        variant="filled"
+                        inputRightAddon={(
+                            <IconButton
+                                aria-label="send_button"
+                                icon={(!isLoading) ? (<FiSend />) : (<Spinner />)}
+                                backgroundColor="transparent"
+                                onClick={handleSubmit(handleAsk)}
+                            />
+                        )}
+                        {...register('input')}
+                        onSubmit={console.log}
+                        onKeyDown={(e) => {
+                            if (e.key == "Enter") {
+                                handleAsk({ input: e.currentTarget.value })
+                            };
+                        }}
+                    />
+                    <Text
+                        textAlign="center"
+                        fontSize="sm"
+                        opacity={.5}
+                    >Free Research Preview. Our goal is to make AI systems more natural and safe to interact with. Your feedback will help us improve.</Text>
+                </Stack>
             </Stack>
         </Stack>
     );
